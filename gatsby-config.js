49c52ca2ffa3ca7d9a04c055abd2e434d63e3917
Sudharaka Palamakumbura <sudharakap@mun.ca>
2020-03-28 16:29:08 -0700

    cleanup of plugin manifest and add favicon.svg
    add a favicon based on the current logo, cleanup of files and cleanup of plugin manifest
    
    Fixes #18

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
     title: `CherishCakes`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    description: `Custom cake designs for every occasion.`,
+    author: `OpenArchitex`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
@@ -18,13 +18,13 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
+        name: `CherishCakes`,
+        short_name: `CherishCakes`,
         start_url: `/`,
         background_color: `#663399`,
         theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        icon: `src/images/favicon.svg`, // This path is relative to the root of the site.
       },
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality
