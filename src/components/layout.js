bd2b9e7ea70b4f52f4e63ec86a03fb7b2c1c21e5
Sudharaka Palamakumbura <sudharakap@mun.ca>
2020-03-27 21:49:53 -0700

    add some of the website sections
    Add Jumbotron, Contact Us, About Us and Footer sections of the website and add Intellij IDEA directory to .gitignore
    
    Fixes #3, #4, #6, #7

--- src/components/layout.js
+++ src/components/layout.js
@@ -10,6 +10,8 @@ import PropTypes from "prop-types"
 import { useStaticQuery, graphql } from "gatsby"
 import Header from "./header"
 import "./layout.css"
+import JumbotronElement from "./jumbotron"
+import Footer from "./footer";
 
 const Layout = ({ children }) => {
   const data = useStaticQuery(graphql`
@@ -25,20 +27,17 @@ const Layout = ({ children }) => {
   return (
     <>
       <Header siteTitle={data.site.siteMetadata.title} />
+      <JumbotronElement/>
       <div
         style={{
           margin: `0 auto`,
-          maxWidth: 960,
+          maxWidth: 1000,
           padding: `0 1.0875rem 1.45rem`,
         }}
       >
         <main>{children}</main>
-        <footer>
-          © {new Date().getFullYear()}, Built with
-          {` `}
-          <a href="https://www.gatsbyjs.org">Gatsby</a>
-        </footer>
       </div>
+      <Footer/>
     </>
   )
 }
---
+++ src/components/main-content.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const MainContent = () => (
+    <div></div>
+)
+
+export default MainContent
