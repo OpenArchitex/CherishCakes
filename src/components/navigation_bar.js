6ebb8307b36a0ed8125dd112e2f5fa7e0f8c4d8f
Faith Lee <faithghlee@gmail.com>
2020-03-28 20:49:55 -0400

    first mock up navigation bar using react-bootsrap nav bar

--- src/components/navigation_bar.js
+++ src/components/navigation_bar.js
@@ -1,25 +1,28 @@
 import React, {Component} from 'react';
 import { Nav, Navbar} from 'react-bootstrap';
-import logo from "../images/cake_logo.svg"
+import logo from "../images/default.png"
+import "./layout.css"
 class Navigation_Bar extends Component {
   render() {
     return (
-        <Navbar bg="light" variant="light">
+        <Navbar bg="#FFFFFF" variant="light" expand='lg'>
             <Navbar.Brand href="#">
             <img
                 src={logo}
-                width="30"
-                height="30"
+                width="300"
+                height="50"
                 className="d-inline-block align-top"
                 alt=""
             />{``}
-            CherishCakes
             </Navbar.Brand>
+            <Navbar.Toggle aria-controls="basic-navbar-nav" />
+            <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="ml-auto">
-            <Nav.Link href="#aboutUs">About Us</Nav.Link>
-            <Nav.Link href="#menu">FAQ</Nav.Link>
-            <Nav.Link href="#contactUs">Contact Us</Nav.Link>
+                <Nav.Link href="#aboutus" className="w3-myfont">About Us</Nav.Link>
+                <Nav.Link href="#faq" className = "w3-myfont">FAQ</Nav.Link>
+                <Nav.Link href="#contactus" className="w3-myfont">Contact Us</Nav.Link>
             </Nav>
+            </Navbar.Collapse>
         </Navbar>
     );
   }
