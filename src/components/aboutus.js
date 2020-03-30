b7802defbf40d8a664261c601e3bc769df6112fb
Sudharaka Palamakumbura <sudharakap@mun.ca>
2020-03-29 17:39:49 -0700

    minor typo mistake correction and add formatting to layout.css
    minor typo mistake on aboutus.js and format layout.css
    
    Related to #19

--- src/components/aboutus.js
+++ src/components/aboutus.js
@@ -1,15 +1,15 @@
 import React from "react";
-import logo from "../images/about-us.jpg"
+import PhotoOfCreator from "../images/about-us.jpg"
 
 const AboutUs = () => (
     <div className="container-fluid mt-5 mb-5" id="about-us">
         <div className="row">
             <div className="col-xs-12 col-sm-6">
                 <img
-                    src={logo}
+                    src={PhotoOfCreator}
                     className="d-inline-block align-top mb-0"
                     width="400"
-                    alt="Woman Holding Cake"
+                    alt="About the Creator"
                 />
             </div>
             <div className="col-xs-12 col-sm-5 order-first">
@@ -23,7 +23,7 @@ const AboutUs = () => (
                     produce uniquely crafted custom cakes especially for you!
                 </p>
                 <p>
-                    I would love to make your perfect custom cake! <a href="#contact-us">Please contact me</a> to to get started!
+                    I would love to make your perfect custom cake! <a href="#contact-us">Please contact me</a> to get started!
                 </p>
             </div>
         </div>
