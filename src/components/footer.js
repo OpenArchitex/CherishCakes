ec692c2bcb83505348feaf286e5e53cc75507d1e
Sudharaka Palamakumbura <sudharakap@mun.ca>
2020-05-12 19:56:26 -0700

    add instagram and fb account links

--- src/components/footer.js
+++ src/components/footer.js
@@ -9,15 +9,15 @@ const Footer = () => (
                 <div className="row">
                     <div className="mx-auto">
                         <ListGroup horizontal>
-                            <ListGroup.Item className="border-0"><FontAwesomeIcon icon={['fab', 'twitter']} size="2x"/></ListGroup.Item>
-                            <ListGroup.Item className="border-0"><FontAwesomeIcon icon={['fab', 'facebook']} size="2x"/></ListGroup.Item>
-                            <ListGroup.Item className="border-0"><FontAwesomeIcon icon={['fab', 'instagram']} size="2x"/></ListGroup.Item>
+                            {/*<ListGroup.Item className="border-0"><FontAwesomeIcon icon={['fab', 'twitter']} size="2x"/></ListGroup.Item>*/}
+                            <ListGroup.Item className="border-0"><a style={{color: "inherit"}} href="https://www.facebook.com/priyangasg/" target="_blank" rel="noopener noreferrer"><FontAwesomeIcon icon={['fab', 'facebook']} size="2x"/></a></ListGroup.Item>
+                            <ListGroup.Item className="border-0"><a style={{color: "inherit"}} href="https://www.instagram.com/priyangasg/" target="_blank" rel="noopener noreferrer"><FontAwesomeIcon icon={['fab', 'instagram']} size="2x"/></a></ListGroup.Item>
                         </ListGroup>
                     </div>
                 </div>
                 <p>
                     <small>
-                        Powered by <a href="https://github.com/OpenArchitex/CherishCakes" target="_blank" rel="noopener noreferrer">CherishCakes</a>
+                        Powered by <a href="https://github.com/OpenArchitex" target="_blank" rel="noopener noreferrer">OpenArchitex</a>
                         &copy;{new Date().getFullYear()}. Code licensed under an {` `}
                         <a href="https://opensource.org/licenses/AGPL-3.0" target="_blank" rel="noopener noreferrer">AGPL-3.0 License</a>.
                     </small>
