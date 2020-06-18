27b7e896125d071a35963348148e9273105a9b5b
Faith Lee <faithghlee@gmail.com>
2020-06-18 07:05:29 -0400

    gallery.js

---
+++ src/pages/gallery.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+import NavBar from "../components/nav-bar";
+import { Gallery } from "gatsby-theme-gallery";
+
+const MyGallery = () => (
+  <NavBar>
+      <Gallery />
+
+  </NavBar>
+)
+
+export default MyGallery
