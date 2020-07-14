ffd64a9751155a147cfbbe7265045b5ba115d12e
Sudharaka Palamakumbura <sudharakap@mun.ca>
2020-07-13 22:11:40 -0700

    feat: change to MIT license

--- src/components/footer.js
+++ src/components/footer.js
@@ -18,7 +18,7 @@ const Footer = () => (
                     <small>
                         Powered by <a href="https://github.com/OpenArchitex" target="_blank" rel="noopener noreferrer">OpenArchitex</a>
                         &copy;{new Date().getFullYear()}. Code licensed under an {` `}
-                        <a href="https://opensource.org/licenses/AGPL-3.0" target="_blank" rel="noopener noreferrer">AGPL-3.0 License</a>.
+                        <a href="https://opensource.org/licenses/MIT" target="_blank" rel="noopener noreferrer">MIT</a>.
                     </small>
                 </p>
             </div>
