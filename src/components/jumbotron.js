3247f1ddb073e27910483dfbdd0b3bb0422963b5
Sudharaka <sudharakap@mun.ca>
2024-02-09 18:39:23 -0800

    feat: upgrade fully all dependencies

--- src/components/jumbotron.js
+++ src/components/jumbotron.js
@@ -1,9 +1,8 @@
 import React from "react"
-import Jumbotron from "react-bootstrap/Jumbotron"
 import Container from "react-bootstrap/Container"
 import { Carousel, CarouselItem } from "react-bootstrap"
 import { graphql, useStaticQuery } from "gatsby"
-import Img from "gatsby-image"
+import { GatsbyImage } from "gatsby-plugin-image"
 
 const JumbotronElement = () => {
   const query = useStaticQuery(graphql`
@@ -21,58 +20,52 @@ const JumbotronElement = () => {
 
     fragment squareImage on File {
       childImageSharp {
-        fluid(maxWidth: 1600, maxHeight: 900) {
-          ...GatsbyImageSharpFluid
-        }
+        gatsbyImageData(layout: FULL_WIDTH)
       }
     }
   `)
 
   return (
-    <Jumbotron fluid className="p-0">
-      <Container fluid className="p-0">
-        <Carousel indicators={false}>
-          <CarouselItem>
-            <Img
-              fluid={query.image1.childImageSharp.fluid}
-              alt={query.image1.relativePath}
-            />
-            <Carousel.Caption>
-              <h3>Custom Cakes to Cherish Every Moment!</h3>
-              <p>
-                Cherish the beautiful memories for sometimes these are the
-                blessings that you are left with.
-              </p>
-            </Carousel.Caption>
-          </CarouselItem>
-          <CarouselItem>
-            <Img
-              fluid={query.image2.childImageSharp.fluid}
-              alt={query.image2.relativePath}
-            />
-            <Carousel.Caption>
-              <h3>Let Us Make Your Wedding Cake!</h3>
-              <p>
-                Life is a cake and love is the icing on top of it. Without love,
-                it becomes difficult to swallow life.
-              </p>
-            </Carousel.Caption>
-          </CarouselItem>
-          <CarouselItem>
-            <Img
-              fluid={query.image3.childImageSharp.fluid}
-              alt={query.image3.relativePath}
-            />
-            <Carousel.Caption>
-              <h3>Cakes For Every Occasion!</h3>
-              <p>
-                Where there is cake, there is hope. And there is always cake.
-              </p>
-            </Carousel.Caption>
-          </CarouselItem>
-        </Carousel>
-      </Container>
-    </Jumbotron>
+    <Container fluid className="p-0">
+      <Carousel indicators={false}>
+        <CarouselItem>
+          <GatsbyImage
+            image={query.image1.childImageSharp.gatsbyImageData}
+            alt={query.image1.relativePath}
+          />
+          <Carousel.Caption>
+            <h3>Custom Cakes to Cherish Every Moment!</h3>
+            <p>
+              Cherish the beautiful memories for sometimes these are the
+              blessings that you are left with.
+            </p>
+          </Carousel.Caption>
+        </CarouselItem>
+        <CarouselItem>
+          <GatsbyImage
+            image={query.image2.childImageSharp.gatsbyImageData}
+            alt={query.image2.relativePath}
+          />
+          <Carousel.Caption>
+            <h3>Let Us Make Your Wedding Cake!</h3>
+            <p>
+              Life is a cake and love is the icing on top of it. Without love,
+              it becomes difficult to swallow life.
+            </p>
+          </Carousel.Caption>
+        </CarouselItem>
+        <CarouselItem>
+          <GatsbyImage
+            image={query.image3.childImageSharp.gatsbyImageData}
+            alt={query.image3.relativePath}
+          />
+          <Carousel.Caption>
+            <h3>Cakes For Every Occasion!</h3>
+            <p>Where there is cake, there is hope. And there is always cake.</p>
+          </Carousel.Caption>
+        </CarouselItem>
+      </Carousel>
+    </Container>
   )
 }
 
--- src/pages/gallery.js
+++ src/pages/gallery.js
@@ -3,11 +3,11 @@ import React from "react"
 import SEO from "../components/seo"
 import Layout from "../components/layout"
 import Gallery from "@browniebroke/gatsby-image-gallery"
-import "@browniebroke/gatsby-image-gallery/dist/style.css"
 import { graphql } from "gatsby"
 
 const ImageGallery = ({ data, location }) => {
-  const images = data.images.edges.map((obj) => obj.node.childImageSharp)
+  const images = data.images.edges.map(({ node }) => node.childImageSharp)
+  console.log(images)
   return (
     <Layout location={location}>
       <SEO title="Gallery" />
@@ -20,17 +20,17 @@ export const query = graphql`
   query ImagesForGallery {
     images: allFile(
       filter: { relativeDirectory: { eq: "cake-gallery" } }
-      sort: { fields: name }
+      sort: { name: ASC }
     ) {
       edges {
         node {
           childImageSharp {
-            thumb: fluid(maxWidth: 270, maxHeight: 270) {
-              ...GatsbyImageSharpFluid
-            }
-            full: fluid(maxWidth: 1024) {
-              ...GatsbyImageSharpFluid
-            }
+            thumb: gatsbyImageData(
+              width: 270
+              height: 270
+              placeholder: BLURRED
+            )
+            full: gatsbyImageData(layout: FULL_WIDTH)
           }
         }
       }
